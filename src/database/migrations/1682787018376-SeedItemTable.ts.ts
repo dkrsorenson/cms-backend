13f8f8c35475110e4d6caf7a763cab58cf807ec0
Dakota Sorenson <dkrsorenson@gmail.com>
2023-04-29 19:14:49 -0400

    Updating seed script to reflect the new item table name change

--- src/database/migrations/1682787018376-SeedItemTable.ts.ts
+++ src/database/migrations/1682787018376-SeedItemTable.ts.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm'
 export class SeedItemTable1682787018376 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        INSERT INTO "items" ("title", "description", "is_active")
+        INSERT INTO "item" ("title", "description", "is_active")
             VALUES('Testing', 'Testing 12345', true);
     `)
   }
