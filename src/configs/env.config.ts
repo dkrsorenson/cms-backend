01032bdf072f6bf1374c891d21ec02097393a073
Dakota Sorenson <dkrsorenson@gmail.com>
2023-05-02 22:08:50 -0400

    Removing unused database-related env variables

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -13,7 +13,6 @@ type ENV = {
   DB_DATABASE: string | undefined
   DB_PASSWORD: string | undefined
   DB_PORT: number | undefined
-  DB_POOL_SIZE: number | undefined
   JWT_SECRET: string | undefined
   JWT_EXPIRATION: string | undefined
 }
@@ -27,7 +26,6 @@ type Config = {
   DB_DATABASE: string
   DB_PASSWORD: string
   DB_PORT: number
-  DB_POOL_SIZE: number
   JWT_SECRET: string
   JWT_EXPIRATION: string
 }
@@ -42,7 +40,6 @@ const getConfig = (): ENV => {
     DB_DATABASE: process.env.DB_DATABASE ? String(process.env.DB_DATABASE) : undefined,
     DB_PASSWORD: process.env.DB_PASSWORD ? String(process.env.DB_PASSWORD) : undefined,
     DB_PORT: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-    DB_POOL_SIZE: process.env.DB_POOL_SIZE ? Number(process.env.DB_POOL_SIZE) : undefined,
     JWT_SECRET: process.env.JWT_SECRET ? String(process.env.JWT_SECRET) : undefined,
     JWT_EXPIRATION: process.env.JWT_EXPIRATION ? String(process.env.JWT_EXPIRATION) : undefined,
   }
